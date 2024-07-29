b46596d4c59e3633979c7488441272304590325a
Igor <105680110+Igor-Mateus-da-Silva@users.noreply.github.com>
2024-07-29 20:30:50 -0300

    Adicionar arquivos de projeto.

---
+++ CursoCSharp/Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CursoCSharp.Fundamentos {
+    internal class PrimeiroPrograma {
+        static void Main() {
+            Console.Write("Primeiro ");
+            Console.WriteLine("Programa");
+            Console.WriteLine("Terminou!");
+        }
+    }
+}
