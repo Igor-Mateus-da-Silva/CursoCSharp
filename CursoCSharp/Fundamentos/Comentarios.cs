9ca9a2250b26ab5b5d80df608d5bf3bfde2cc5ea
Igor <105680110+Igor-Mateus-da-Silva@users.noreply.github.com>
2024-07-30 13:55:13 -0300

    add fundamentos

---
+++ CursoCSharp/Fundamentos/Comentarios.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CursoCSharp.Fundamentos {
+    /// <summary>
+    /// XML Comment
+    /// </summary>
+    internal class Comentarios {
+        public static void Executar() {
+            // Cuidado com os comentários desnecessários, que não agregam valor
+            Console.WriteLine("Código claro é sempre melhor!");
+            /* 
+             * Comentário de multiplas linhas 
+             */
+            Console.WriteLine("O C# tem o XML Comments");
+        }
+    }
+}
