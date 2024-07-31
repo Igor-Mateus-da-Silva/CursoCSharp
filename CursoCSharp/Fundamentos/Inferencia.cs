72642cabcae9420a562c9965775b4c18066c4aab
Igor <105680110+Igor-Mateus-da-Silva@users.noreply.github.com>
2024-07-30 22:31:34 -0300

    +Fundamentos

---
+++ CursoCSharp/Fundamentos/Inferencia.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos {
+    internal class Inferencia {
+        public static void Executar() {
+            var nome = "Igor";
+            // nome = 123;
+            Console.WriteLine(nome);
+
+            // int idade;
+            var idade = 28;
+            Console.WriteLine(idade);
+
+            int a;
+            a = 3;
+
+            int b = 2;
+
+            Console.WriteLine(a + b);
+
+        }
+    }
+}
