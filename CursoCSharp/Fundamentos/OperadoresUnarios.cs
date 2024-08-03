4680a50975ab97aaeddda00d895efd00456b9d9f
Igor <105680110+Igor-Mateus-da-Silva@users.noreply.github.com>
2024-08-03 08:50:11 -0300

    +fundamentos_v6

---
+++ CursoCSharp/Fundamentos/OperadoresUnarios.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos {
+    internal class OperadoresUnarios {
+        public static void Executar () {
+
+        }
+    }
+}
