594f9440534b2c6e28ef4919d3bee6ce179a9d72
Igor <105680110+Igor-Mateus-da-Silva@users.noreply.github.com>
2024-08-05 14:04:46 -0300

    +estrutura_v1 & change [att] `fundamentos`

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 
 using CursoCSharp.Fundamentos;
+using CursoCSharp.EstruturaDeControle;
 
 namespace CursoCSharp {
     class Program {
         static void Main(string[] args) {
             var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                // Fundamentos
                 {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
                 {"Comentários - Fundamentos", Comentarios.Executar},
                 {"Váriaveis e Constantes - Fundamentos", VariaveisEConstantes.Executar},
@@ -22,6 +24,13 @@ namespace CursoCSharp {
                 {"Operadores Atribuição - Fundamentos", OperadoresAtribuicao.Executar},
                 {"Operadores Unários - Fundamentos", OperadoresUnarios.Executar},
                 {"Operador Ternário - Fundamentos", OperadorTernario.Executar},
+                
+                // Estruturas de Controle
+                {"Estrutura If - Estrutura de Controle", EstruturaIf.Executar},
+                {"Estrutura If/Else - Estrutura de Controle", EstruturaIfElse.Executar},
+                {"Estrutura If/Else/If - Estrutura de Controle", EstruturaIfElseIf.Executar},
+                {"Estrutura Switch - Estrutura de Controle", EstruturaSwitch.Executar},
+                {"Estrutura While - Estrutura de Controle", EstruturaWhile.Executar},
             });
 
             central.SelecionarEExecutar();
