5d48eebe0d20496a589a8801c5c6aa2e3b0de83a
Igor <105680110+Igor-Mateus-da-Silva@users.noreply.github.com>
2024-08-14 14:57:28 -0300

    +class&methods_v1

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 using CursoCSharp.Fundamentos;
 using CursoCSharp.EstruturasDeControle;
+using CursoCSharp.ClassesEMetodos;
 
 namespace CursoCSharp {
     class Program {
@@ -36,6 +37,9 @@ namespace CursoCSharp {
                 {"Estrutura For Each - Estrutura de Controle", EstruturaForEach.Executar},
                 {"Usando Break - Estrutura de Controle", UsandoBreak.Executar},
                 {"Usando Continue - Estrutura de Controle", UsandoContinue.Executar},
+
+                // Classes e Metodos
+                {"Membros - Classes e Métodos", Membros.Executar},
             });
 
             central.SelecionarEExecutar();
