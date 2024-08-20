0d99be24861a49eb3726a0a5996f659c3dd556c4
Igor <105680110+Igor-Mateus-da-Silva@users.noreply.github.com>
2024-08-20 14:03:42 -0300

    +class&methods_v4

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -44,6 +44,10 @@ namespace CursoCSharp {
                 {"Métodos com Retorno - Classes e Métodos", MetodosComRetorno.Executar},
                 {"Métodos Estaticos - Classes e Métodos", MetodosEstaticos.Executar},
                 {"Atributos Estaticos - Classes e Métodos", AtributosEstaticos.Executar},
+                {"Desafio Atributo - Classes e Métodos", DesafioAtributo.Executar},
+                {"Params - Classes e Métodos", Params.Executar},
+                {"Parametros Nomeados - Classes e Métodos", ParametrosNomeados.Executar},
+                {"Get Set - Classes e Métodos", GetSet.Executar},
             });
 
             central.SelecionarEExecutar();
