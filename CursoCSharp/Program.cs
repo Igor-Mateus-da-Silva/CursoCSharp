cdd879f89226ebd389d6ae3727f355ad12696640
Igor <105680110+Igor-Mateus-da-Silva@users.noreply.github.com>
2024-09-09 14:00:53 -0300

    +oo_v3

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -73,6 +73,9 @@ namespace CursoCSharp {
                 {"Herança - OO", Heranca.Executar},
                 {"Construtor This - OO", ConstrutorThis.Executar},
                 {"Encapsulamento - OO", OO.Encapsulamento.Executar},
+                {"Polimorfismo - OO", Polimorfismo.Executar},
+                {"Abstract - OO", Abstract.Executar},
+                {"Interface - OO", Interface.Executar},
             });
 
             central.SelecionarEExecutar();
